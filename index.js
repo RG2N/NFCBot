f2e9a3f655363dce517b881b74d93410d13b110a
RG2N <64289295+RG2N@users.noreply.github.com>
2021-08-09 23:11:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+require('dotenv').config(); //initialize dotenv
+const Discord = require('discord.js'); //import discord.js
 token = process.env.token
+
+const client = new Discord.Client(); //create new client
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//make sure this line is the last line
+client.login(token); //login bot using token
