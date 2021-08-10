e1adb26cd935cd1775c2217c54b6dcf103d38d9a
RG2N <64289295+RG2N@users.noreply.github.com>
2021-08-09 23:41:50 -0400

    a

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
+const status = require('minecraft-server-status');
 token = process.env.token
 
-const client = new Discord.Client(); //create new client
+const bot = new Discord.Client(); //create new bot
+const prefix = "!";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+  bot.user.setPresence({
+    status: 'online',
+    activity: {
+        name: 'with depression',
+        type: 'STREAMING',
+        url: 'https://www.twitch.tv/monstercat'
+    }
+})
 });
 
+bot.on('message', msg => {
+    command(msg);
+});
+
+function command(msg) {
+    if (msg.content === prefix + 'help') {
+        msg.channel.send('Coming Soon.');
+    }
+}
+
 //make sure this line is the last line
-client.login(token); //login bot using token
+bot.login(token); //login bot using token
